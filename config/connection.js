3a6c0ef28a4a764d73c7af1ee7e78b1fe4158970
san-geetha-s <101059528+san-geetha-s@users.noreply.github.com>
2022-07-16 12:58:21 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url=`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.qvpmm.mongodb.net/shopping?retryWrites=true&w=majority`
+//     const url=`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.qvpmm.mongodb.net/shopping?retryWrites=true&w=majority`
 //    const url=' mongodb+srv://sangeetha:<password>@cluster0.cxlnuak.mongodb.net/?retryWrites=true&w=majority'
-    // const url = 'mongodb://localhost:27017';
+    const url = 'mongodb://localhost:27017';
     const dbname='shopping'
 
 
